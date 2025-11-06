6920e30a660baa4204a659b7a346535c75a6f6a6
ro1oc <x2315340@outlook.com>
2025-11-07 04:40:37 +0800

    更新 rename_to_xigua.js

--- rename_to_xigua.js
+++ rename_to_xigua.js
@@ -1,6 +1,7 @@
-module.exports = async (nodes, utils) => {
-  nodes.forEach((node, index) => {
-    node.name = `西瓜专线-${index + 1}`
-  })
-  return nodes
+function operator(proxies, targetPlatform) {
+  const _ = lodash;
+  return proxies.map((p, i) => {
+    _.set(p, 'name', `西瓜专线${i + 1}`); // 序号从1开始
+    return p;
+  });
 }
